7caec5a32b1010f36e190ed814ea7f2445998863
andisiambuku <roselandambuku@gmail.com>
2021-06-22 11:22:45 +0300

    second commit

--- lib/user_details.dart
+++ lib/user_details.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 class UserDetails extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+       resizeToAvoidBottomInset : true,
+     body: SingleChildScrollView(
+      child:  Container(
       padding: EdgeInsets.all(20.0),
       child: Column(
         children: [
@@ -16,6 +19,7 @@ class UserDetails extends StatelessWidget {
                   color: Color(0xff1dbf73),
                 )),
           ),
+           SizedBox(height: 5),
           TextField(
             keyboardType: TextInputType.text,
             decoration: InputDecoration(
@@ -25,6 +29,7 @@ class UserDetails extends StatelessWidget {
                   color: Color(0xff1dbf73),
                 )),
           ),
+           SizedBox(height: 5),
           TextField(
               keyboardType: TextInputType.emailAddress,
               decoration: InputDecoration(
@@ -33,12 +38,14 @@ class UserDetails extends StatelessWidget {
                   labelStyle: TextStyle(
                     color: Color(0xff1dbf73),
                   ))),
+                   SizedBox(height: 5),
           TextField(
               keyboardType: TextInputType.text,
               decoration: InputDecoration(
                   labelText: 'Username',
                   border: OutlineInputBorder(),
                   labelStyle: TextStyle(color: Color(0xff1dbf73)))),
+                   SizedBox(height: 5),
           TextField(
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
@@ -46,6 +53,7 @@ class UserDetails extends StatelessWidget {
                   border: OutlineInputBorder(),
                   labelStyle: TextStyle(color: Color(0xff1dbf73)),
                  )),
+                  SizedBox(height: 5),
           TextField(
               keyboardType: TextInputType.number,
               decoration: InputDecoration(
@@ -53,108 +61,15 @@ class UserDetails extends StatelessWidget {
                   border: OutlineInputBorder(),
                   labelStyle: TextStyle(color: Color(0xff1dbf73)),
                   )),
+                   SizedBox(height: 10),
           ElevatedButton(
               child: Text('UPDATE'),
               onPressed: () {},
                 ),    
         ],
       ),
-    );
-
-    /*return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            children: [
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    TextField(
-                      maxLength: 20,
-                keyboardType:TextInputType.text,
-                      decoration: InputDecoration(
-                          labelText: 'First Name',
-                          labelStyle: TextStyle(
-                            color: Color(0xff1dbf73),
-                            
-                          )
-                          ),
-                    ),
-                    TextField(
-                        keyboardType:TextInputType.text,
-                        maxLength: 20,
-                      decoration: InputDecoration(
-                          labelText: 'Last Name',
-                          labelStyle: TextStyle(
-                            color: Color(0xff1dbf73),
-                          )),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    TextField(
-                keyboardType:TextInputType.emailAddress,
-                maxLength: 100,
-                        decoration: InputDecoration(
-                            labelText: 'Email',
-                            labelStyle: TextStyle(
-                              color: Color(0xff1dbf73),
-                            ))),
-                    TextField(
-                        keyboardType:TextInputType.text,
-                        maxLength: 20,
-                        decoration: InputDecoration(
-                            labelText: 'Username',
-                            labelStyle: TextStyle(color: Color(0xff1dbf73)))),
-                  ],
-                ),
-              ),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    TextField(
-                        keyboardType:TextInputType.number,
-                        maxLength: 20,
-                        decoration: InputDecoration(
-                            labelText: 'Phone Number',
-                            labelStyle: TextStyle(
-                              color: Color(0xff1dbf73)),
-                              enabledBorder: UnderlineInputBorder(
-                       borderSide: BorderSide(color: Color(0xff1dbf73)),
-                               )
-                              )),
-                    TextField(
-                        keyboardType:TextInputType.number,
-                        maxLength: 20,
-                        decoration: InputDecoration(
-                            labelText: 'ID / Passport',
-                            labelStyle: TextStyle(color: Color(0xff1dbf73)),
-                        enabledBorder: UnderlineInputBorder(
-                       borderSide: BorderSide(color: Color(0xff1dbf73)),
-                             )
-                            )),
-                  ],
-                ),
-              ),
-
-
-              Container(
-                child: ElevatedButton(
-                  child: Text('UPDATE'),
-                  onPressed: () {},
-                ),
-              ),
-            ],
-          ),
-        ),
       ),
-    );*/
+     ),
+    );
   }
 }
--- lib/user_location.dart
+++ lib/user_location.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 class UserLocation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+     return Scaffold(
+       resizeToAvoidBottomInset : true,
+     body: SingleChildScrollView(
+      child:  Container(
       padding: EdgeInsets.all(20.0),
-      child: Column(children: [
+      child: Column(
+        children: [
         TextField(
             keyboardType: TextInputType.text,
             decoration: InputDecoration(
@@ -14,6 +18,7 @@ class UserLocation extends StatelessWidget {
                 labelStyle: TextStyle(
                   color: Color(0xff1dbf73),
                 ))),
+                 SizedBox(height: 10),
         TextField(
             keyboardType: TextInputType.text,
             decoration: InputDecoration(
@@ -22,130 +27,20 @@ class UserLocation extends StatelessWidget {
                 labelStyle: TextStyle(
                   color: Color(0xff1dbf73),
                 ))),
+                 SizedBox(height: 10),
         TextField(
             keyboardType: TextInputType.streetAddress,
             decoration: InputDecoration(
                 labelText: 'Postal Code or Zip Code',
                 border: OutlineInputBorder(),
                 labelStyle: TextStyle(color: Color(0xff1dbf73)))),
+                 SizedBox(height: 10),
         ElevatedButton(
           child: Text('UPDATE'),
           onPressed: () {},
         ),
       ]),
+      ))
     );
-    /* return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          children: [
-            Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    TextField(
-                keyboardType:TextInputType.text,
-                      maxLength: 20,
-                      decoration: InputDecoration(
-                        
-                          labelText: 'Current Location',
-                          labelStyle: TextStyle(
-                            color: Color(0xff1dbf73),
-                          )),
-                    ),
-
-                  ],
-                ),
-              ),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    TextField(
-                keyboardType:TextInputType.text,
-                maxLength: 20,
-                        decoration: InputDecoration(
-                            labelText: 'Street Name',
-                            labelStyle: TextStyle(
-                              color: Color(0xff1dbf73),
-                            ))),
-                    TextField(
-                        keyboardType:TextInputType.streetAddress,
-                        maxLength: 20,
-                        decoration: InputDecoration(
-                            labelText: 'Postal Code or Zip Code',
-                            labelStyle: TextStyle(color: Color(0xff1dbf73)))),
-                  ],
-                ),
-              ),
-
-              Container(
-                child: ElevatedButton(
-                  child: Text('UPDATE'),
-                  onPressed: () {},
-                ),
-              ),
-        ],
-        ),
-      ),
-    );*/
-    /*return Scaffold(
-       body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            children: [
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    TextField(
-                keyboardType:TextInputType.text,
-                      maxLength: 20,
-                      decoration: InputDecoration(
-                        
-                          labelText: 'Current Location',
-                          labelStyle: TextStyle(
-                            color: Color(0xff1dbf73),
-                          )),
-                    ),
-
-                  ],
-                ),
-              ),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                   TextField(
-                keyboardType:TextInputType.text,
-                maxLength: 20,
-                        decoration: InputDecoration(
-                            labelText: 'Street Name',
-                            labelStyle: TextStyle(
-                              color: Color(0xff1dbf73),
-                            ))),
-                    TextField(
-                        keyboardType:TextInputType.streetAddress,
-                        maxLength: 20,
-                        decoration: InputDecoration(
-                            labelText: 'Postal Code or Zip Code',
-                            labelStyle: TextStyle(color: Color(0xff1dbf73)))),
-                  ],
-                ),
-              ),
-
-              Container(
-                child: ElevatedButton(
-                  child: Text('UPDATE'),
-                  onPressed: () {},
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );*/
   }
 }
